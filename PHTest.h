33127d09140a6998e5e19c37b0402d65f3e99eb0
Patrick Healey <pgh225@nyu.edu>
2015-06-26 19:03:41 -0400

    added a test class

---
+++ PHTest.h
@@ -0,0 +1,13 @@
+//
+//  PHTest.h
+//  bfGitTest
+//
+//  Created by Patrick Healey on 6/26/15.
+//  Copyright (c) 2015 Pat Healey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PHTest : NSObject
+
+@end
---
+++ PHTest.m
@@ -0,0 +1,13 @@
+//
+//  PHTest.m
+//  bfGitTest
+//
+//  Created by Patrick Healey on 6/26/15.
+//  Copyright (c) 2015 Pat Healey. All rights reserved.
+//
+
+#import "PHTest.h"
+
+@implementation PHTest
+
+@end
